dc3c9ea23ca97d592c09504e6df3d97bb295f899
Jordan England-Nelson <jordan.england.nelson@gmail.com>
2018-12-28 11:08:41 -0800

    Updates

---
+++ precache-manifest.b99931a77e8f964fa9ba8d0aab95ce07.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "356d3b7ca9dd92d571a764df7bfc0bbf",
+    "url": "/street-art-talks/static/media/shepard_fairey_reagan.356d3b7c.mp3"
+  },
+  {
+    "revision": "08826acf73adc2a0c44950c48e60ace6",
+    "url": "/street-art-talks/static/media/shepard-fairey-reagan.08826acf.jpg"
+  },
+  {
+    "revision": "b3df6d1582b9f0b50abc3b56626b15be",
+    "url": "/street-art-talks/static/media/arts-district-dtla-skyline.b3df6d15.jpg"
+  },
+  {
+    "revision": "e5af3c69390cdeaa6165dd3784419dc3",
+    "url": "/street-art-talks/static/media/angelina-christina-1.e5af3c69.jpg"
+  },
+  {
+    "revision": "3e211daf316f155af075df628471c5d9",
+    "url": "/street-art-talks/static/media/AngelinaChristina1.3e211daf.mp3"
+  },
+  {
+    "revision": "26098869fb1d387d979f",
+    "url": "/street-art-talks/static/js/runtime~main.26098869.js"
+  },
+  {
+    "revision": "a29c58b8ec6425e69e05",
+    "url": "/street-art-talks/static/js/main.a29c58b8.chunk.js"
+  },
+  {
+    "revision": "72845526bdcd5c5e333c",
+    "url": "/street-art-talks/static/js/1.72845526.chunk.js"
+  },
+  {
+    "revision": "a29c58b8ec6425e69e05",
+    "url": "/street-art-talks/static/css/main.9ff1d5ba.chunk.css"
+  },
+  {
+    "revision": "c472d8b9448e9b323cdf24feedfc8426",
+    "url": "/street-art-talks/index.html"
+  }
+];
