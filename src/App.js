dd369380a35213ad1a13a88cd193b492dd8f7644
Jordan England-Nelson <jordan.england.nelson@gmail.com>
2018-12-28 10:33:11 -0800

    Adds Shepard Fairey Reagan mural card

--- src/App.js
+++ src/App.js
@@ -2,15 +2,25 @@ import React, { Component } from 'react';
 import skyline from './img/arts-district-dtla-skyline.jpg';
 import angelinaChristina1 from './img/angelina-christina-1.jpg';
 import angelinaChristina1_audio from './audio/AngelinaChristina1.mp3'
+import shepard_fairey_reagan_img from './img/shepard-fairey-reagan.jpg';
+import shepard_fairey_reagan_audio from './audio/shepard_fairey_reagan.mp3';
 import './App.css';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 const MuralList = () => (
   <div>
-    <Link to={process.env.PUBLIC_URL + '/angelina-christina'}>
-      <img src={angelinaChristina1} />
-      <div>Two faces</div>
-    </Link>
+    <div className="mural-card">
+      <Link to={process.env.PUBLIC_URL + '/angelina-christina'}>
+        <img src={angelinaChristina1} />
+        <div>Two faces</div>
+      </Link>
+    </div>
+    <div className="mural-card">
+      <Link to={process.env.PUBLIC_URL + '/shepard-fairey-reagan'}>
+        <img src={shepard_fairey_reagan_img} />
+        <div>Legislative Influence For Sale</div>
+      </Link>
+    </div>
   </div>
 )
 
@@ -29,7 +39,7 @@ const Home = () => (
   </div>
 )
 
-const MuralDetail = (props) => (
+const MuralCard = (props) => (
   <div>
     <img src={props.image}/>
     <div>{props.title}</div>
@@ -56,12 +66,25 @@ class App extends Component {
               exact
               path={process.env.PUBLIC_URL + '/angelina-christina'}
               render={(props) => (
-                <MuralDetail {...props}
+                <MuralCard {...props}
                              name="Angelina Christina"
                              image={angelinaChristina1}
                              title="Two faces embracing."
                              location="4th and Merrick."
                              audio={angelinaChristina1_audio}
+                />
+              )}
+            />
+            <Route
+              exact
+              path={process.env.PUBLIC_URL + '/shepard-fairey-reagan'}
+              render={(props) => (
+                <MuralCard {...props}
+                             name="Shepard Fairey"
+                             image={shepard_fairey_reagan_img}
+                             title="Legislative Influence For Sale."
+                             location="Alemeda St and Traction Ave."
+                             audio={shepard_fairey_reagan_audio}
                 />
               )}
             />
